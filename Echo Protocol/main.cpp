3b90289ab1b7286966f0ed0c0d36caac94583169
Fev1L <demchvlad17@gmail.com>
2026-01-10 20:43:29 +0100

    Initial Commit

---
+++ Echo Protocol/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Echo Protocol
+//
+//  Created by Fev1L on 10.01.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
